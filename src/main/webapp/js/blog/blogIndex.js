9b71579a710c5bac4b5def1af7517d4e584a8465
jackiezz <jackiezz@163.com>
2019-08-02 00:12:59 +0800

    领域数据查询前端实现

--- src/main/webapp/js/blog/blogIndex.js
+++ src/main/webapp/js/blog/blogIndex.js
@@ -1,5 +1,14 @@
 $(function () {
-    $.get("/user/getLoginUser", "", function (data) {
+    // 发送请求请求领域名称
+    $.get("/field/findAll", {}, function (data) {
+        var btns = '';
+        for (var i = 0; i < data.length; i++) {
+            var btn = '<button type="button" class="btn btn-secondary bg-success ">'+data[i].fname+'</button>'
+            btns += btn;
+        }
+        btns += '<button type="button" class="btn btn-secondary bg-success"><img src="../../images/icon_img/plus-2x.png"></button>';
 
+        $("#fieldList").html(btns)
     })
+
 });
