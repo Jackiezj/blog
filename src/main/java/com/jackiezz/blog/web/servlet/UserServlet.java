5b01f53249ede6a1c80cb72cb20e6479b6e68f5a
jackiezz <jackiezz@163.com>
2019-08-01 11:09:00 +0800

    BaseServlet的抽取和使用  * 所有的Servlet都由BaseServlet实现路由分发  * 所有Servlet  *  定义方法: @WebServlet("/xxx/*")  *  定义函数, 路由如: /xxx/addUser将自动执行Servlet中的addUser函数

---
+++ src/main/java/com/jackiezz/blog/web/servlet/UserServlet.java
@@ -0,0 +1,15 @@
+package com.jackiezz.blog.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/*")
+public class UserServlet extends BaseServlet {
+    public void register (HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
