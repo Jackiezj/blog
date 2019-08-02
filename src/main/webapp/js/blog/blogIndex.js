06c0e7d47489cba147bfb68599c9fa5fa28b8fdb
jackiezz <jackiezz@163.com>
2019-08-02 10:46:52 +0800

    分类数据查询前端实现

--- src/main/webapp/js/blog/blogIndex.js
+++ src/main/webapp/js/blog/blogIndex.js
@@ -3,12 +3,34 @@ $(function () {
     $.get("/field/findAll", {}, function (data) {
         var btns = '';
         for (var i = 0; i < data.length; i++) {
-            var btn = '<button type="button" class="btn btn-secondary bg-success ">'+data[i].fname+'</button>'
+            var btn = '<button type="button" class="btn btn-secondary bg-success fbtn" id=f"' + data[i].fid + '">' + data[i].fname + '</button>'
             btns += btn;
         }
         btns += '<button type="button" class="btn btn-secondary bg-success"><img src="../../images/icon_img/plus-2x.png"></button>';
 
-        $("#fieldList").html(btns)
+        $("#fieldList").html(btns);
+    })
+
+    // 默认请求id为1的field的category发送请求请求分类名称
+    findAllCategory(1);
+    // 点击filed时加载对应category的name
+    $(".fbtn").click(function () {
+        var fbtnId = $(this).attr("id");
+        fbtnId = fbtnId.substring(1);
+        findAllCategory(fbtnId);
     })
 
 });
+
+function findAllCategory(fid) {
+    // 根据fid查询所有的category
+    $.get("/category/findAll", {"fid": fid}, function (data) {
+        var btns = '<button type="button" class="btn btn-secondary">New</button>';
+        for (var i = 0; i < data.length; i++) {
+            var btn = '<button type="button" class="btn btn-secondary" cid="' + data[i].cid + '">data[i].cname</button>';
+        }
+        btns += btn;
+        $("#categoryList").html(btns);
+    })
+
+}
